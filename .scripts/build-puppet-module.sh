ea25de96b51d5403982f77cb20a53bd40c510a0b
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2014-02-02 17:57:47 +0100

    fixed build-puppet-module script

--- .scripts/build-puppet-module.sh
+++ .scripts/build-puppet-module.sh
@@ -10,8 +10,8 @@ TARGET=`mktemp -d`
 
 function do_build_module {
   tag=$1
-  tgz="$TARGET/ptomulik-repos-${tag}.tar.gz"
-  dir="ptomulik-repos-$tag/"
+  tgz="$TARGET/ptomulik-oval-${tag}.tar.gz"
+  dir="ptomulik-oval-$tag/"
   git archive --prefix $dir --output $tgz $tag
   (cd $TARGET && tar -xzf $tgz && cd $TARGET/$dir && puppet module build)
 }
